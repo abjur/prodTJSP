5ea34ad87eb08e4fa7b3189e43418f59f4e2d3e5
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-28 10:28:41 -0300

    documents courts data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Court list data
+#'
+#' Contains court list data obtained from TJSP productivity site.
+#'
+#' @format A data frame with 2941 rows and 4 variables:
+#' \describe{
+#'    \item{id_comarca}{Internal id of the comarca}
+#'    \item{nm_comarca}{Name of the comarca}
+#'    \item{id_court}{Internal id of the court}
+#'    \item{nm_court}{Name of the court}
+#' }
+#' @source \url{http://www.tjsp.jus.br/produtividadeweb/}
+"courts"
