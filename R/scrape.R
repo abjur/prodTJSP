c736048111f3d389b8460b14e697cc4b7993ebe0
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-28 20:15:23 -0300

    added pdf parse to court data

--- R/scrape.R
+++ R/scrape.R
@@ -18,3 +18,92 @@ prod_scrape_courts <- function(h) {
     purrr::discard(~.x=='')
   tibble::tibble(nm_court = nms, id_court = ids)
 }
+
+# parsers de pdf --------------------------------------------------------------
+
+prod_parse_uni_one <- function(arq) {
+  txt <- pdftools::pdf_text(arq)
+  re_mag <- 'Dados +do\\(s\\) +Magistrados *\\(s\\)'
+  re_unidade <- 'Dados da Unidade'
+  re_banned <- c("^TJ/SP", "^Movimento Judici\\u00e1rio", "^Refer\\u00eancia:",
+                 "^Foro:", "^Unidade:", "^Planilha:", "^Foro:", "^$",
+                 "^\\u00c1REA:", "^FASE PR", "^J\\u00c1 AJUIZ") %>%
+    stringr::str_c(collapse = '|')
+  re_id <- '^[0-9]+\\.([A-Z0-9]+)?(\\.[A-Z0-9]+)*'
+  re_num <- "[0-9]*$|At[e\\u00e9] +[0-9]+ +m[\\u00eae]s(es)?$|N\\u00e3o se aplica$"
+  re_num_notnull <- "[0-9]+$|At[e\\u00e9] +[0-9]+ +m[\\u00eae]s(es)?$|N\\u00e3o se aplica$"
+  re_txt_split <- '(  |$| [0-9]$)'
+  re_txt <- '^[^ ]+ ([A-Z].+)'
+  txt_clean <- txt %>%
+    paste(collapse = '@@@') %>%
+    stringr::str_sub(stringr::str_locate(., re_unidade)[1, 2] + 1,
+                     stringr::str_locate(., re_mag)[1, 1] - 1) %>%
+    stringr::str_replace_all('[0-9]+\n@@@', '') %>%
+    stringr::str_split('\n') %>%
+    unlist() %>%
+    purrr::discard(~toupper(.x) == .x &
+                     stringr::str_detect(.x, '[A-Za-z]')) %>%
+    stringr::str_trim() %>%
+    purrr::discard(stringr::str_detect, pattern = re_banned)
+  if (all(is.na(txt_clean))) return(tibble::tibble(result = 'vazio'))
+  ids <- txt_clean %>% stringr::str_extract(re_id)
+  nas_num <- which(is.na(ids) & stringr::str_detect(txt_clean, '^[0-9]+$'))
+  nas_txt <- which(is.na(ids) &
+                     !stringr::str_detect(txt_clean, '^[0-9]+$') &
+                     stringr::str_length(txt_clean) < 30) # ver vara_2011_12_075_003086
+  nas_anterior <- which(is.na(ids)[-1] &
+                          !is.na(dplyr::lag(ids[-1])) &
+                          !stringr::str_detect(dplyr::lag(txt_clean[-1]),
+                                               re_num_notnull))
+  txt_clean[nas_anterior] <- paste(
+    txt_clean[nas_anterior],
+    txt_clean[nas_txt],
+    txt_clean[nas_num]
+  )
+  txt_clean <- txt_clean[!is.na(ids)]
+  ids <- ids[!is.na(ids)]
+  nums <- txt_clean %>%
+    stringr::str_extract(re_num) %>%
+    stringr::str_trim()
+  txts <- txt_clean %>%
+    stringr::str_split_fixed(re_txt_split, 2) %>%
+    magrittr::extract(TRUE, 1) %>%
+    stringr::str_match(re_txt) %>%
+    magrittr::extract(TRUE, 2) %>%
+    stringr::str_trim()
+  tab <- tibble::tibble(id = ids, txt = txts,
+                        num = nums, result = 'OK')
+  tab
+}
+
+#' Check if file was read correctly
+#'
+#' Does some tests to check if a file was read correctly.
+#'
+#' @param arq name of the file
+#'
+#' @export
+arq_check <- function(arq) {
+  d <- prod_parse_uni_one(arq)
+  if (all(d$result %in% c('erro'))) cat('Erro: ', arq, '\n')
+  if (all(d$result %in% c('vazio', 'erro'))) return(TRUE)
+  titulos <- d %>%
+    dplyr::filter(num == '') %>%
+    with(id) %>%
+    purrr::discard(stringr::str_detect, pattern = '^[0-9]+\\.$')
+  length(titulos) == 0
+}
+
+#' Court productivity data
+#'
+#' Parses pdf file to get Court productivity data. Ignores information about the
+#' judge.
+#'
+#' @param arqs character vector contaning the paths of the pdf files.
+#'
+#' @export
+prod_parse_uni <- function(arqs) {
+  abjutils::dvec(prod_parse_uni_one, arqs) %>%
+    dplyr::rename(arq = item)
+}
+
